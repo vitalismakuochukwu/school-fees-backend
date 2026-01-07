bd2a2b1256706be485ccb2c1787c21b2f4dda4ef
Yvitalismakuochukw <vitalismakuochukwu@gmail.com>
2026-01-07 20:20:41 +0100

    Fix: Add Admin routes, controller, and Fee model for automatic updates

--- models/Fee.js
+++ models/Fee.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const feeSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    required: true,
-    default: 45500
-  }
-}, { timestamps: true });
+const FeeSchema = new mongoose.Schema({
+  level: { type: String, required: true, unique: true },
+  amount: { type: Number, required: true },
+  session: { type: String, default: "2025/2026" }
+});
 
-module.exports = mongoose.model('Fee', feeSchema);
+module.exports = mongoose.model('Fee', FeeSchema);
